9880539dd5f8d89b6760749d25921223537a6916
Vbif322 <hxdfue@yandex.ru>
2022-11-17 13:05:07 +0300

    banners get colors from props

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -3,15 +3,16 @@ import './style.css';
 
 const Banner = ({title, info, color, img}) => {
   return (
-    <div className="banner-wrapper">
-        <div className="banner-text">
-            <h3>{title}</h3>
-            <p>{info}</p>
+        <div className="banner__content" style={{background: color}}>
+            <div className="banner__text">
+                <h3>{title}</h3>
+                <p>{info}</p>
+            </div>
+            <div className="banner__img">
+                <img src={img} alt={title} />
+            </div>
         </div>
-        <div className="banner-img">
-            <img src={img} alt={title} />
-        </div>
-    </div>
+
   )
 }
 
